a4f5251c90be98ae6993a88735253e7826ce5e46
mahonya <nightkid@ya.ru>
2021-02-27 20:21:24 +0700

    Redirect 404 => index

--- app.py
+++ app.py
@@ -1,10 +1,17 @@
-from flask import Flask
+from flask import Flask, redirect, url_for
 
 app = Flask(__name__)
+
 
 @app.route('/')
 def index():
     return 'Hello World'
+
+
+@app.errorhandler(404)
+def notfound():
+    return redirect(url_for('index'))
+
 
 if __name__ == "__main__":
     app.run()
