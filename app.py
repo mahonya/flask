bdf2f01b8c0b66c6659655ba9c0529b272786d64
chemyakin <chemyakin@uniscan.biz>
2021-03-04 14:15:28 +0700

    favicon

--- app.py
+++ app.py
@@ -1,6 +1,15 @@
-from flask import Flask, redirect, url_for, render_template
+from flask import (
+    Flask, redirect, url_for, render_template, send_from_directory)
+import os
 
 app = Flask(__name__)
+
+
+@app.route('/favicon.ico')
+def favicon():
+    return send_from_directory(os.path.join(app.root_path, 'static'),
+                               'favicon.ico', 
+                               mimetype='image/vnd.microsoft.icon')
 
 
 @app.route('/')
